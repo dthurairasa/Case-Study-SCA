f47c90bf4c1f9b2d0e3bfb32c0504b53e01c8bc2
Lara <lara.untch@tu-berlin.de>
2025-07-24 20:14:37 +0200

    UI with item and time seperation and extra changing divs

--- DispositionAnalysis/server.R
+++ DispositionAnalysis/server.R
@@ -14,17 +14,8 @@ shinyServer(function(input, output, session) {
   ## ------------------------------------------
   ## 0) Hilfs-Reaktivitäten (Werk & Material)
   ## ------------------------------------------
-  # Drop-down „Werk“ füllen, sobald orders da ist
-  observe({
-    req(orders)
-    werke <- sort(unique(orders$WERKS))
-    
-    updateSelectInput(session, "werk",
-                      choices  = werke,
-                      selected = first(werke))
-  })
   
-  # Material-Liste für zweites Drop-down (falls noch nicht im UI)
+  # Material-Liste für Drop-down
   observe({
     req(orders)
     mats <- sort(unique(orders$MATNR))
@@ -37,10 +28,9 @@ shinyServer(function(input, output, session) {
   ## 1) Daten nach Werk & Zeitraum filtern
   ## ------------------------------------------
   df_filtered <- reactive({
-    req(input$werk, input$zeitraum)
+    req(input$zeitraum)
     orders %>%
       filter(
-        WERKS        == input$werk,
         Lieferdatum >= input$zeitraum[1],
         Lieferdatum <= input$zeitraum[2]
       )
@@ -68,30 +58,25 @@ shinyServer(function(input, output, session) {
   output$kpi_lead  <- renderText( sprintf("%.1f d",  kpi_vals()$ltime) )
   
   ## ------------------------------------------
-  ## 3) Histogramm der Durchlaufzeiten
+  ## 3) Extra-Information
   ## ------------------------------------------
-  output$durchlaufPlot <- renderPlot({
-    ggplot(df_filtered(), aes(x = Durchlaufzeit)) +
-      geom_histogram(binwidth = 1) +
-      labs(title = "Verteilung der Durchlaufzeiten",
-           x = "Durchlaufzeit (Tage)",
-           y = "Anzahl Aufträge") +
-      theme_minimal()
+  selected_kpi <- reactiveVal(NULL)
+  
+  observeEvent(input$kpi_ifr, {
+    selected_kpi("Item Information")
+  })
+  observeEvent(input$kpi_time, {
+    selected_kpi("Time Information")
   })
   
-  ## ------------------------------------------
-  ## 4) Tabelle
-  ## ------------------------------------------
-  output$dtTable <- renderDataTable(
-    df_filtered(),
-    options = list(pageLength = 10, autoWidth = TRUE)
-  )
+  output$kpi_info <- renderUI({
+    text <- if (is.null(selected_kpi())) {
+      "Please select the field you are interested in"
+    } else {
+      selected_kpi()
+    }
+    tags$div(style = "text-align: center;", text)
+  })
   
-  ## ------------------------------------------
-  ## 5) Download
-  ## ------------------------------------------
-  output$downloadData <- downloadHandler(
-    filename = function() paste0("Prozessanalyse_", Sys.Date(), ".xlsx"),
-    content  = function(file) writexl::write_xlsx(df_filtered(), path = file)
-  )
+  
 })
